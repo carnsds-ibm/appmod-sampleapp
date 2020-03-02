f5a18b67309338257d9a021f4c1ad0ffa178c46c
Dillon Carns <Dillon.Carns@ibm.com>
2020-03-02 10:06:34 -0500

    Initial commit

---
+++ src/main/java/dev/appsody/comic/Application.java
@@ -0,0 +1,9 @@
+package dev.appsody.comic;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/home")
+public class Application extends Application {
+    
+}
